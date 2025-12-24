71337b66b3003a1656b05b349b6628cc813faf35
n1c0145 <nicolas.lozalml@gmail.com>
2025-12-24 10:58:58 -0500

    add new controller for home

---
+++ app/Http/Controllers/Reporting/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Reporting;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Models\Ticket;
+use App\Models\Project;
+use App\Models\TicketThread;
+use App\Models\TicketThreadHistory;
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    //
+}
